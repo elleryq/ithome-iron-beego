5b3ecc73785b708f1ab73ac18b245dd0ceb1374c
elleryq <elleryq@gmail.com>
2019-09-11 14:34:51 +0000

    Add CRUD comments

--- main.go
+++ main.go
@@ -37,5 +37,61 @@ func init() {
 }
 
 func main() {
+	// Create
+	/*
+		userNotAdded := models.User{Name: "John Doe", Birthday: time.Now(), Gender: "M"}
+		userid, err := models.AddUser(&userNotAdded)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(userid)
+	*/
+
+	// Query
+	/*
+		query := map[string]string{}
+		fields := []string{}
+		sortby := []string{}
+		order := []string{}
+		users, err := models.GetAllUser(query, fields, sortby, order, 0, -1)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(users)
+	*/
+
+	// Retrieve single object
+	/*
+		user, err := models.GetUserById(2)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(user)
+	*/
+
+	// Update
+	/*
+		user.Name = "Mary Jane"
+		user.Gender = "F"
+		err = models.UpdateUserById(user)
+		if err != nil {
+			panic(err)
+		}
+		// Retrieve again to verify
+		user, err = models.GetUserById(2)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(user)
+	*/
+
+	// Delete
+	/*
+		err := models.DeleteUser(2)
+		if err != nil {
+			panic(err)
+		}
+	*/
+
 	beego.Run()
 }
