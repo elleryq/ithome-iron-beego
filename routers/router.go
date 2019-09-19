f6f4713aa5e627ea9bc7d549f55a6f58c9529563
elleryq <elleryq@gmail.com>
2019-09-19 15:46:23 +0000

    Add Basic authentication

--- routers/router.go
+++ routers/router.go
@@ -4,11 +4,14 @@ import (
 	"my/hello/controllers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/auth"
 )
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/about", &controllers.AboutController{})
+
+	beego.InsertFilter("/myuser/*", beego.BeforeRouter, auth.Basic("foobar", "pass"))
 	beego.Router("/myuser", &controllers.MyUserController{}, "get:GetAll")
 	beego.Router("/myuser/create", &controllers.MyUserController{}, "get:GetAddForm")
 	beego.Router("/myuser/create", &controllers.MyUserController{}, "post:PostAddForm")
