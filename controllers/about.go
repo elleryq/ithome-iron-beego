47d20bb1709940d292daa1948b6777fe349b80cf
elleryq <elleryq@gmail.com>
2019-09-19 14:57:14 +0000

    Test Session

--- controllers/about.go
+++ controllers/about.go
@@ -15,6 +15,16 @@ func (c *AboutController) URLMapping() {
 }
 
 func (c *AboutController) Get() {
+	key := "asta"
+	v := c.GetSession(key)
+	if v == nil {
+		c.SetSession(key, int(1))
+		c.Data[key] = 0
+	} else {
+		c.SetSession(key, v.(int)+1)
+		c.Data[key] = v.(int)
+	}
+
 	c.Data["Name"] = "John Doe"
 	c.TplName = "about.tpl"
 }
