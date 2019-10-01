fb2522ee5aee068b964212f74569fd850366d1f5
elleryq <elleryq@gmail.com>
2019-10-01 09:53:59 +0000

    Add model and migration

---
+++ database/migrations/20191001_071922_post.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Post_20191001_071922 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Post_20191001_071922{}
+	m.Created = "20191001_071922"
+
+	migration.Register("Post_20191001_071922", m)
+}
+
+// Run the migrations
+func (m *Post_20191001_071922) Up() {
+	// use m.SQL("CREATE TABLE ...") to make schema update
+	m.SQL("CREATE TABLE post(`id` int(11) NOT NULL AUTO_INCREMENT,`member_id` int(11) DEFAULT NULL,`title` varchar(128) NOT NULL,`content` text NULL,`posted_at` datetime NOT NULL,`modified_at` datetime NOT NULL,PRIMARY KEY (`id`),FOREIGN KEY(MemberId) REFERENCES member(Id))")
+}
+
+// Reverse the migrations
+func (m *Post_20191001_071922) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+	m.SQL("DROP TABLE `post`")
+}
