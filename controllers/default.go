e324738a407de3eee52df003c75b7c87c4a1c1db
elleryq <elleryq@gmail.com>
2019-10-02 07:03:23 +0000

    Make home page to display latest 10 posts

--- controllers/default.go
+++ controllers/default.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/utils/pagination"
+	"github.com/elleryq/ithome-iron-beego/models"
 )
 
 type MainController struct {
@@ -9,7 +12,29 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	var fields []string
+	var sortby []string
+	var order []string
+	var query = make(map[string]string)
+
+	sortby = []string{"PostedAt"}
+	order = []string{"desc"}
+	postsPerPage := 10
+	postCount, err := models.CountPost()
+	if err != nil {
+	}
+	paginator := pagination.SetPaginator(c.Ctx, postsPerPage, postCount)
+
+	// fetch the next 20 posts
+	posts, err := models.GetAllPost(query, fields, sortby, order, int64(paginator.Offset()), int64(postsPerPage))
+	if err != nil {
+		logs.Error(err.Error())
+	}
+	c.Data["posts"] = posts
+	/*
+		post := posts[0].(models.Post)
+		logs.Debug("1.", post.Title)
+		logs.Debug("2.", post.Member.Username)
+	*/
 	c.TplName = "index.tpl"
 }
